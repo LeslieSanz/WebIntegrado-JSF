c5f09d621560d4c23608837c88a3b952c72e591f
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2024-07-13 22:56:33 -0500

    Agregando imagenes de los cursos

--- src/main/java/controlador/control_curso.java
+++ src/main/java/controlador/control_curso.java
@@ -5,8 +5,11 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import beans.*;
 import dao.Negocio;
+import java.io.IOException;
 import java.util.*;
 import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 @ManagedBean
 @RequestScoped
@@ -16,23 +19,43 @@ public class control_curso {
     private List<Curso> liscur;
     private List<SelectItem> liscat; //Para el comboBox de Categorias
     private Categoria c;
+    private Curso cur;
     
     
     //Constructor
     public control_curso() {
      c = new Categoria();
+     cur = new Curso();
      liscur=new ArrayList();
     }
     
-    //Filtrar cursos por Categorias
-    public void filtraCat(){
-       liscur = new Negocio().ListarCursosPorCategoria(c.getCod());
-   }
+    public void filtraCat() {
+        System.out.println("Entrando a filtraCat()");
+        try {
+            liscur = new Negocio().ListarCursosPorCategoria(c.getCod());
+        } catch (Exception e) {
+            System.out.println("Error al filtrar cursos por categoría: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+     // Método para buscar un curso por código
+    public void buscarCurso(String cod) {
+        System.out.println("Entrando a buscarCurso() con código: " + cod);
+        try {
+            cur = new Negocio().buscarCurso(cod);
+        } catch (Exception e) {
+            System.out.println("Error al buscar curso por código: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    
     
     @PostConstruct  // Se ejecuta cuando la pagina jsf se levanta
     public void init() {
         try {
-            
+            //System.out.println("Jaja");
            //Para listar todos los cursos
            liscur = new Negocio().ListarCursos();
 
@@ -42,12 +65,18 @@ public class control_curso {
                 SelectItem cat = new SelectItem(x.getCod(), x.getNombre());
                 liscat.add(cat);
             }
+            
+            String cod = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("cod");
+            if (cod != null) {
+                buscarCurso(cod);
+            }
 
         } catch (Exception e) {
             e.printStackTrace(); // Imprime la traza del error para más detalles
         }
     }
 
+    // Getters y Setters
     public List<SelectItem> getLiscat() {
         return liscat;
     }
@@ -70,6 +99,14 @@ public class control_curso {
 
     public void setLiscur(List<Curso> liscur) {
         this.liscur = liscur;
+    }
+
+    public Curso getCur() {
+        return cur;
+    }
+
+    public void setCur(Curso cur) {
+        this.cur = cur;
     }
     
 }
--- src/main/java/dao/Negocio.java
+++ src/main/java/dao/Negocio.java
@@ -12,6 +12,7 @@ public class Negocio {
     public List<Categoria> ListarCategorias(){
         List<Categoria> lista=new ArrayList();
         Connection cn=MySQLConexion.getConexion();
+        System.out.println("Holi este es listar cat");
         try{
             String sql="SELECT codCat,nomCategor FROM categoria";
             PreparedStatement st=cn.prepareStatement(sql);
@@ -31,6 +32,7 @@ public class Negocio {
     public List<Curso> ListarCursos(){
         List<Curso> lista=new ArrayList();
         Connection cn=MySQLConexion.getConexion();
+        System.out.println("Holi este es listar cursos");
         try{
             String sql="SELECT codCurso, nombreCurso, precio, Categoria_codCat, descripCur FROM curso";
             PreparedStatement st=cn.prepareStatement(sql);
@@ -53,6 +55,7 @@ public class Negocio {
     public List<Curso> ListarCursosPorCategoria(String codCat) {
     List<Curso> lista = new ArrayList<>();
     Connection cn = MySQLConexion.getConexion();
+    System.out.println("Holi este es listar cursos por cat");
     try {
         String sql = "SELECT codCurso, nombreCurso, precio, Categoria_codCat,  descripCur FROM curso WHERE Categoria_codCat = ?";
         PreparedStatement st = cn.prepareStatement(sql);
@@ -71,6 +74,30 @@ public class Negocio {
             ex.printStackTrace();
         }
         return lista;
+    }
+    
+    //Buscar por Codigo
+     public Curso buscarCurso(String cod){
+        Curso c = null;
+        Connection cn=MySQLConexion.getConexion();
+         System.out.println("Holi este es buscar curso");
+        try{
+            String sql="SELECT codCurso, nombreCurso, precio, Categoria_codCat,  descripCur FROM curso where codCurso=?";
+            PreparedStatement st=cn.prepareStatement(sql);
+            st.setString(1, cod);
+            ResultSet rs=st.executeQuery();
+            while(rs.next()){
+                c=new  Curso();
+                c.setCodcur(rs.getString(1));
+                c.setNomCur(rs.getString(2));
+                c.setPrecio(rs.getDouble(3));
+                c.setCodCat(rs.getString(4));
+                c.setDescripCur(rs.getString(5));
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return c;
     }
      
 }
